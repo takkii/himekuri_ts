cf8f81a265415b123b8060e303f48437e2b4a059
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-11-15 11:13:36 +0900

    Update, himekuri.

--- himekuri.ts
+++ himekuri.ts
@@ -2,6 +2,7 @@ const week = ["日","月","火","水","木","金","土"];
 const OneDay = new Date();
 const comma = " : ";
 const str = "時刻を表示";
+const OneYear = OneDay.getFullYear();
 
 const TimeNow = str + comma + OneDay.getFullYear() + "年" +
   (OneDay.getMonth() + 1)  + "月" +
@@ -10,4 +11,19 @@ const TimeNow = str + comma + OneDay.getFullYear() + "年" +
   OneDay.getMinutes() + "分" +
   OneDay.getSeconds() + "秒" + comma +
   week[OneDay.getDay()] + "曜日"
+
+const TwoYear = OneYear + 1;
+const newYear= new Date(TwoYear+"/1/1").valueOf();
+const newDays = new Date().valueOf();
+const ThreeYear = OneYear - 2018;
+const newTwoDay = ((OneDay.getMonth() + 1)  + "月" + (OneDay.getDate() + "日"));
+const dot = ".";
+const newThreeDay = ((OneDay.getMonth() + 1)  + dot + (OneDay.getDate()));
+const day = Math.ceil((newYear-newDays)/(60*60*24*1000));
+const himekuri_version = "1.0.0"
+const version = "日めくりバージョン" + comma + himekuri_version 
+
 console.log(TimeNow);
+console.log("来年の1月1日まであと" + comma + day + "日です");
+console.log("令和" + ThreeYear + "年" + newTwoDay + comma + "R0" + ThreeYear + dot +  newThreeDay);
+console.log(version);
