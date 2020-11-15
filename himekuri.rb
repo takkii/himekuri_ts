040f8673787066e22a7b675ed5c4412603e8f40b
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-11-15 13:59:23 +0900

    Update, auto shell.

---
+++ himekuri.rb
@@ -0,0 +1,3 @@
+# $HOME/.nvm/versions/node/v14.15.0/bin/tsc is nvm use node.js version is v14.15.0.
+system("$HOME/.nvm/versions/node/v14.15.0/bin/tsc himekuri.ts", exception: true)
+system("node himekuri.js", exception: true)
