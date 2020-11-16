18e20dd5260bb419dcfa7a61adbec0926e023249
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-11-16 16:04:50 +0900

    Update, version auto add.

--- lib/himekuri.rb
+++ lib/himekuri.rb
@@ -5,12 +5,11 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 require_relative 'himekuri_ts/version'
 
-# rubygem update change.
-# typescript_path and nodejs_path.
-# hiemkuri_ts-x.x.x.x, vesion info.
+version = (HimekuriTs::VERSION).to_s
+himekuri_ts = "himekuri_ts-".to_s + version.to_s
 
-typescript_path = "$HOME/.nvm/versions/node/v14.15.0/bin/tsc $HOME/.rbenv/versions/2.7.2/lib/ruby/gems/2.7.0/gems/himekuri_ts-1.0.3/lib/himekuri.ts".to_s
-nodejs_path = "node $HOME/.rbenv/versions/2.7.2/lib/ruby/gems/2.7.0/gems/himekuri_ts-1.0.3/lib/himekuri.js".to_s
+typescript_path = "$HOME/.nvm/versions/node/v14.15.0/bin/tsc $HOME/.rbenv/versions/2.7.2/lib/ruby/gems/2.7.0/gems/" + himekuri_ts + "/lib/himekuri.ts".to_s
+nodejs_path = "node $HOME/.rbenv/versions/2.7.2/lib/ruby/gems/2.7.0/gems/" + himekuri_ts + "/lib/himekuri.js".to_s
 
 system(typescript_path, exception: true)
 system(nodejs_path, exception: true)
