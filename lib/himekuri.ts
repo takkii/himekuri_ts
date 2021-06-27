b2f0fbc94d422c9742d0d4926d14237121013996
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-06-27 18:01:46 +0900

    Update, 和風月 add.

--- lib/himekuri.ts
+++ lib/himekuri.ts
@@ -21,7 +21,36 @@ const dot = ".";
 const newThreeDay = ((OneDay.getMonth() + 1)  + dot + (OneDay.getDate()));
 const day = Math.ceil((newYear-newDays)/(60*60*24*1000) -1);
 
-console.log(TimeNow);
+const MonthWareki = (OneDay.getMonth()+1)
+
+if (MonthWareki == 1){
+      console.log(TimeNow + comma + "睦月");
+    } else if (MonthWareki == 2){
+        console.log(TimeNow + comma + "如月");
+    } else if (MonthWareki == 3){
+        console.log(TimeNow + comma + "弥生");
+    } else if (MonthWareki == 4){
+        console.log(TimeNow + comma + "卯月");
+    } else if (MonthWareki == 5){
+        console.log(TimeNow + comma + "皐月");
+    } else if (MonthWareki == 6){
+        console.log(TimeNow + comma + "水無月");
+    } else if (MonthWareki == 7){
+        console.log(TimeNow + comma + "文月");
+    } else if (MonthWareki == 8){
+        console.log(TimeNow + comma + "葉月");
+    } else if (MonthWareki == 9){
+        console.log(TimeNow + comma + "長月");
+    } else if (MonthWareki == 10){
+        console.log(TimeNow + comma + "神無月");
+    } else if (MonthWareki == 11){
+        console.log(TimeNow + comma + "霜月");
+    } else if (MonthWareki == 12){
+        console.log(TimeNow + comma + "師走");
+} else {
+  console.log("警告 : ありえない月です！JavaScriptの記述を確認願います！");
+}
+
 console.log("来年の1月1日まであと" + comma + day + "日です");
 
 // R0 → R10 = R010 | R10 Change
