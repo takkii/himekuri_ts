8e32c557e7c2fe84cc1ef25ab442f9c59ef1ea84
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-06-21 15:14:38 +0900

    Update.

--- lib/himekuri_web_days.ts
+++ lib/himekuri_web_days.ts
@@ -1,14 +1,28 @@
-function himekuri_web_days(){
+class Himekuri{
+	OneDay: Date;
+	OneYear: any;
+	TwoYear: any;
+	newYear: number;
+	newDays: number;
+	day: number;
+	comma: string;
 
-const OneDay = new Date();
-const OneYear = OneDay.getFullYear();
-const TwoYear = OneYear + 1;
-const newYear= new Date(TwoYear+"/1/1").valueOf();
-const newDays = new Date().valueOf();
-const day = Math.ceil((newYear-newDays)/(60*60*24*1000) -1);
-const comma = " : ";
+	constructor(){
+		this.OneDay = new Date();
+		this.OneYear = this.OneDay.getFullYear();
+		this.TwoYear = this.OneYear + 1;
+		this.newYear= new Date(this.TwoYear+"/1/1").valueOf();
+		this.newDays = new Date().valueOf();
+		this.day = Math.ceil((this.newYear-this.newDays)/(60*60*24*1000) -1);
+        this.comma = " : ";
+    }
 
-console.log("来年の1月1日まであと" + comma + day + "日です");
+    days(){
+    	console.log(`来年の1月1日まであと${this.comma}${this.day}日です`);
+    }
 }
 
-himekuri_web_days()
+const himekuri = new Himekuri();
+himekuri.days();
+
+
