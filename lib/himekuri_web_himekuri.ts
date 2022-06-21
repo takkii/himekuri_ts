2f315b5486bacfbe77f4bc250e26319d550b005e
Takayuki Kamiyama <karuma.reason@gmail.com>
2022-06-21 14:49:11 +0900

    Update.

--- lib/himekuri_web_himekuri.ts
+++ lib/himekuri_web_himekuri.ts
@@ -1,19 +1,20 @@
 function himekuri_web_himekuri(){
 
-const OneDay = new Date();
-const OneYear = OneDay.getFullYear();
-const comma = " : ";
-
-const TwoYear = OneYear + 1;
-const newYear= new Date(TwoYear+"/1/1").valueOf();
-const newDays = new Date().valueOf();
-const ThreeYear = OneYear - 2018;
-const newTwoDay = ((OneDay.getMonth() + 1)  + "月" + (OneDay.getDate() + "日"));
-const dot = ".";
-const newThreeDay = ((OneDay.getMonth() + 1)  + dot + (OneDay.getDate()));
-
-// R0 → R10 = R010 | R10 Change
-console.log("令和" + ThreeYear + "年" + newTwoDay + comma + "R0" + ThreeYear + dot +  newThreeDay);
+    const OneDay = new Date();
+    const OneYear = OneDay.getFullYear();
+    const comma = " : ";
+    
+    const ThreeYear = OneYear - 2018;
+    const newTwoDay = ((OneDay.getMonth() + 1)  + "月" + (OneDay.getDate() + "日"));
+    const dot = ".";
+    const newThreeDay = ((OneDay.getMonth() + 1)  + dot + (OneDay.getDate()));
+    
+    // R0 → R10 = R010 | R10 Change
+    if (ThreeYear >= 10){
+        console.log("令和" + ThreeYear + "年" + newTwoDay + comma + "R" + ThreeYear + dot +  newThreeDay);
+    } else {
+    console.log("令和" + ThreeYear + "年" + newTwoDay + comma + "R0" + ThreeYear + dot +  newThreeDay);
+    }
 }
-
+    
 himekuri_web_himekuri()
